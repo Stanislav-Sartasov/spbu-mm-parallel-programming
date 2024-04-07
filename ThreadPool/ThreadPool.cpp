d07cd656103a66b8368e8d8f13e485188da59e3c
Ivan Morozko <ivan.morozko01@gmail.com>
2024-04-08 00:07:27 +0400

    [03.ThreadPool]: Impelemnt continue_with method

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -1,69 +1,92 @@
 #include "ThreadPool.h"
 
-template <Strategy strategy> void ThreadPool<strategy>::worker_thread(int index)
+/* Returns true with 1/n probability */
+bool get_random_with_probability(size_t n) {
+    if (n == 0)
+        return true;
+    thread_local std::mt19937 generator(std::random_device{}());
+    thread_local std::uniform_int_distribution<size_t> distribution(1, n);
+    return distribution(generator) == 1;
+}
+
+template <Strategy strategy>
+void ThreadPool<strategy>::balance_queues(size_t curr, size_t victim) {
+    std::scoped_lock lock(tasks_[curr].get_mutex(), tasks_[victim].get_mutex());
+
+    auto &smaller_queue = tasks_[curr].unsafe_size() < tasks_[victim].unsafe_size() ? tasks_[curr] : tasks_[victim];
+    auto &larger_queue = tasks_[curr].unsafe_size() < tasks_[victim].unsafe_size() ? tasks_[victim] : tasks_[curr];
+
+    // Ensuring at most threshold_ task difference
+    while (larger_queue.unsafe_size() - smaller_queue.unsafe_size() > threshold_) {
+        if (auto task = larger_queue.unsafe_dequeue()) {
+            smaller_queue.unsafe_enqueue(std::move(*task));
+        }
+    }
+}
+
+template <Strategy strategy> void ThreadPool<strategy>::worker_thread(size_t index)
 {
     while (!stop_flag_.test(std::memory_order_relaxed)) {
-        if constexpr (strategy == Strategy::WorkSharing) {
-            std::unique_lock<std::mutex> lock(queue_mutex_);
+        std::function<void()> task;
+        bool found = false;
 
-            condition_.wait(lock, [this] {
-                return stop_flag_.test(std::memory_order_relaxed) ||
-                       !tasks_[0].empty();
-            });
+        // Try own queue first
+        auto opt_task = tasks_[index].dequeue();
+        if (opt_task.has_value()) {
+            task = *opt_task;
+            found = true;
+        } else if constexpr (strategy == Strategy::WorkSharing) {
+            size_t own_queue_size = tasks_[index].size();
+            bool should_rebalance = get_random_with_probability(own_queue_size);
 
-            if (stop_flag_.test(std::memory_order_relaxed) && tasks_[0].empty())
-                return;
-
-            // task_[0] is not empty now, so we can safely call task()
-            tasks_[0].dequeue().value()();
-        } else {
-            std::function<void()> task;
-            bool found = false;
-
-            // Try own queue first
-            auto opt_task = tasks_[index].dequeue();
-            if (opt_task.has_value()) {
-              task = *opt_task;
-              found = true;
-            } else {
-              size_t workers_size = workers_size_.load(std::memory_order_relaxed);
-              for (size_t i = 0; i < workers_size; ++i) {
-                auto opt_task_steal = tasks_[get_index()].dequeue();
-                if (opt_task_steal.has_value()) {
-                  task = *opt_task_steal;
-                  found = true;
-                  break;
+            if (should_rebalance) {
+                size_t victim = get_rng_index();
+                if (victim != index) {
+                    balance_queues(index, victim);
                 }
-              }
             }
+        } else if constexpr (strategy == Strategy::WorkStealing) {
+            size_t workers_size = workers_size_.load(std::memory_order_relaxed);
+            for (size_t i = 0; i < workers_size; ++i) {
+                auto opt_task_steal = tasks_[get_rng_index()].dequeue_top();
+                if (opt_task_steal.has_value()) {
+                    task = *opt_task_steal;
+                    found = true;
+                    break;
+                }
+            }
+        }
 
-            if (found) {
-                task();
-            } else {
-                // No task was found, and this thread will sleep for a short
-                // duration to reduce busy waiting
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            }
+        if (found) {
+            task();
+        } else {
+            // No task was found, and this thread will sleep for a short
+            // duration to reduce busy waiting
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
     }
 }
 
 template <Strategy strategy>
 ThreadPool<strategy>::ThreadPool(size_t threads)
-    : tasks_(strategy == Strategy::WorkSharing ? 1 : threads)
+        : tasks_(threads), start_barrier_(threads + 1) // +1 for the main thread
 {
-    for (size_t i = 0; i < threads; ++i)
-        workers_.emplace_back([this, i] { worker_thread(i); });
-
     workers_size_.store(threads, std::memory_order_relaxed);
+
+    for (size_t i = 0; i < threads; ++i)
+        workers_.emplace_back([this, i] {
+            // Wait for all threads to be ready
+            start_barrier_.arrive_and_wait();
+            worker_thread(i);
+        });
+
+    // Allow all worker threads to start
+    start_barrier_.arrive_and_wait();
 }
 
 template <Strategy strategy> ThreadPool<strategy>::~ThreadPool()
 {
     stop_flag_.test_and_set(std::memory_order_relaxed);
-
-    if constexpr (strategy == Strategy::WorkSharing)
-        condition_.notify_all();
 
     for (std::thread &worker : workers_) {
         if (worker.joinable())
--- ThreadPool/ThreadPool.h
+++ ThreadPool/ThreadPool.h
@@ -7,36 +7,35 @@
 #include <random>
 #include <thread>
 #include <vector>
+#include <barrier>
 
-#include "ThreadSafeQueue.h"
+#include "ThreadSafeDeque.h"
 
 enum class Strategy { WorkSharing, WorkStealing };
 
 template <Strategy strategy> class ThreadPool {
 private:
     std::vector<std::thread> workers_;
-    std::atomic<int> workers_size_{0};
-    std::vector<ThreadSafeQueue<std::function<void()>>> tasks_;
+    std::atomic<size_t> workers_size_{0};
+    std::vector<ThreadSafeDeque<std::function<void()>>> tasks_;
     std::atomic_flag stop_flag_ = ATOMIC_FLAG_INIT;
-    std::condition_variable condition_;
     std::mutex queue_mutex_;
-    std::mt19937 rng{ std::random_device{}()}; // Only used for WorkStealing strategy
-    std::mutex rng_mutex; // Mutex to protect rng access
+    std::mt19937 rng{std::random_device{}()};
+    std::mutex rng_mutex;
+    std::barrier<> start_barrier_; // Barrier to synchronize the start of the threads
 
-    std::condition_variable work_stealing_cond_;
-    std::mutex work_stealing_mutex_;
+    const size_t threshold_ = 2;
 
-    void worker_thread(int index);
+    void balance_queues(size_t index, size_t victim);
 
-    int get_index()
+    void worker_thread(size_t index);
+
+    size_t get_rng_index()
     {
-        if constexpr (strategy == Strategy::WorkSharing)
-            return 0;
-
         std::lock_guard<std::mutex> lock(rng_mutex);
-        int max = workers_size_.load(std::memory_order_relaxed) - 1;
+        size_t max = workers_size_.load(std::memory_order_relaxed) - 1;
 
-        return std::uniform_int_distribution<int>{0, max}(rng);
+        return std::uniform_int_distribution<size_t>{0, max}(rng);
     }
 
 public:
@@ -53,12 +52,38 @@ public:
         std::future<return_type> res = task_ptr->get_future();
         {
             std::unique_lock<std::mutex> lock(queue_mutex_);
-            tasks_[get_index()].enqueue([task_ptr] { (*task_ptr)(); });
+            tasks_[get_rng_index()].enqueue([task_ptr] { (*task_ptr)(); });
         }
 
-        if constexpr (strategy == Strategy::WorkSharing)
-            condition_.notify_one();
-
         return res;
+    }
+
+    template <typename T, typename Cont>
+    auto continue_with(std::future<T> &&future, Cont &&continuation)
+    {
+        using cont_return_type = std::invoke_result_t<Cont, T>;
+
+        auto task_wrapper = [fut = std::move(future),
+                             cont = std::forward<Cont>(continuation)]() mutable {
+            T result = fut.get();
+            return cont(result);
+        };
+
+        // Due to the nature of std::packaged_task, which requires a callable of
+        // signature void(), we need to match this requirement if the
+        // continuation returns void. This adaptation is necessary because the
+        // implementation detail of enqueue expects a no-argument callable.
+        using task_return_type =
+            std::conditional_t<std::is_same_v<cont_return_type, void>,
+                               std::packaged_task<void()>,
+                               std::packaged_task<cont_return_type()>>;
+
+        auto packagedTask =
+            std::make_shared<task_return_type>(std::move(task_wrapper));
+        auto resultFuture = packagedTask->get_future();
+
+        enqueue([packagedTask] { (*packagedTask)(); });
+
+        return resultFuture;
     }
 };
