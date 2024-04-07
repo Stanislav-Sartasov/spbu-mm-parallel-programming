c2ce24d2d6d61aa6b91cba0474a4d3730b739f8b
Ivan Morozko <ivan.morozko01@gmail.com>
2024-04-07 23:40:41 +0400

    [03.ThreadPool]: Make tests check work-stealing strategy too
    Make TYPED_TESTs to force gtest library to generate tests for
    both template arguments

--- ThreadPool/tests.cpp
+++ ThreadPool/tests.cpp
@@ -1,23 +1,38 @@
-#include <gtest/gtest.h>
 #include <algorithm>
+#include <gtest/gtest.h>
 
 #include "ThreadPool.h"
 
-TEST(ThreadPoolTests, SingleTask) {
-    ThreadPool pool(4);
-    std::future<int> result = pool.enqueue([]() { return 1; });
+template<typename T>
+class ThreadPoolTest : public ::testing::Test {
+public:
+    ThreadPoolTest() : pool(4) {}
+protected:
+    ThreadPool<T::value> pool;
+};
+
+using WorkSharingStrategy = std::integral_constant<Strategy, Strategy::WorkSharing>;
+using WorkStealingStrategy = std::integral_constant<Strategy, Strategy::WorkStealing>;
+
+typedef ::testing::Types<WorkSharingStrategy, WorkStealingStrategy> Implementations;
+
+TYPED_TEST_SUITE(ThreadPoolTest, Implementations);
+
+TYPED_TEST(ThreadPoolTest, SingleTask)
+{
+    std::future<int> result = this->pool.enqueue([]() { return 1; });
     ASSERT_EQ(result.get(), 1);
 }
 
-TEST(ThreadPoolTests, MultipleTasks) {
-    ThreadPool pool(4);
-    std::future<int> result1 = pool.enqueue([]() { return 1; });
-    std::future<int> result2 = pool.enqueue([]() { return 2; });
+TYPED_TEST(ThreadPoolTest, MultipleTasks)
+{
+    std::future<int> result1 = this->pool.enqueue([]() { return 1; });
+    std::future<int> result2 = this->pool.enqueue([]() { return 2; });
     ASSERT_EQ(result1.get() + result2.get(), 3);
 }
 
-TEST(ThreadPoolTests, HighVolume) {
-    ThreadPool pool(100);
+template <Strategy strategy> void test_high_volume(ThreadPool<strategy> pool)
+{
     std::vector<std::future<int>> results;
     for (int i = 0; i < 1000; ++i) {
         results.push_back(pool.enqueue([i]() { return i; }));
@@ -29,39 +44,50 @@ TEST(ThreadPoolTests, HighVolume) {
     ASSERT_EQ(sum, 499500); // Sum of first 1000 natural numbers
 }
 
+// This test do not use default 4 pool size
+TEST(ThreadPoolTests, HighVolume)
+{
+    test_high_volume(ThreadPool<Strategy::WorkSharing>(100));
+    test_high_volume(ThreadPool<Strategy::WorkStealing>(100));
+}
+
 // Task with side-effects
-TEST(ThreadPoolTests, SideEffectTask) {
-    ThreadPool pool(4);
+TYPED_TEST(ThreadPoolTest, SideEffectTask)
+{
     std::vector<int> numbers(1000, 0);
     std::vector<std::future<void>> futures;
 
     for (int i = 0; i < 1000; ++i) {
-        futures.push_back(pool.enqueue([&numbers, i]() { numbers[i] = i; }));
+        futures.push_back(
+            this->pool.enqueue([&numbers, i]() { numbers[i] = i; }));
     }
 
-    for (auto& future : futures) {
+    for (auto &future : futures) {
         future.get();
     }
 
     // Check if numbers vector was populated correctly
-    bool all_correct = std::all_of(numbers.begin(), numbers.end(), [idx = 0](int value) mutable { return value == idx++; });
+    bool all_correct =
+        std::all_of(numbers.begin(), numbers.end(),
+                    [idx = 0](int value) mutable { return value == idx++; });
     ASSERT_TRUE(all_correct);
 }
 
 // Tasks with varying execution times
-TEST(ThreadPoolTests, VaryingExecutionTimes) {
-    ThreadPool pool(4);
+TYPED_TEST(ThreadPoolTest, VaryingExecutionTimes)
+{
     std::vector<std::future<int>> futures;
 
     for (int i = 0; i < 10; ++i) {
-        futures.push_back(pool.enqueue([i]() {
-            std::this_thread::sleep_for(std::chrono::milliseconds(50 * i)); // Increasing sleep time
+        futures.push_back(this->pool.enqueue([i]() {
+            // Increasing sleep time
+            std::this_thread::sleep_for(std::chrono::milliseconds(50 * i));
             return i;
         }));
     }
 
     int sum = 0;
-    for (auto& future : futures) {
+    for (auto &future : futures) {
         sum += future.get();
     }
 
@@ -69,53 +95,62 @@ TEST(ThreadPoolTests, VaryingExecutionTimes) {
 }
 
 // Tasks that throw exceptions
-TEST(ThreadPoolTests, ExceptionTask) {
-    ThreadPool pool(4);
-    std::future<void> result = pool.enqueue([]() { throw std::runtime_error("Test exception"); });
+TYPED_TEST(ThreadPoolTest, ExceptionTask)
+{
+    std::future<void> result = this->pool.enqueue(
+        []() { throw std::runtime_error("Test exception"); });
 
-    EXPECT_THROW({
-        try {
-            result.get();
-        } catch (const std::runtime_error& e) {
-            ASSERT_STREQ("Test exception", e.what());
-            throw;
-        }
-    }, std::runtime_error);
+    EXPECT_THROW(
+        {
+            try {
+                result.get();
+            } catch (const std::runtime_error &e) {
+                ASSERT_STREQ("Test exception", e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
 }
 
 // Tasks returning complex data types
-TEST(ThreadPoolTests, ReturnComplexType) {
-    ThreadPool pool(4);
-    std::future<std::vector<int>> result = pool.enqueue([]() -> std::vector<int> {
-        return std::vector<int>{1, 2, 3, 4, 5};
-    });
+TYPED_TEST(ThreadPoolTest, ReturnComplexType)
+{
+    std::future<std::vector<int>> result =
+        this->pool.enqueue([]() -> std::vector<int> {
+            return std::vector<int>{1, 2, 3, 4, 5};
+        });
 
     std::vector<int> expected{1, 2, 3, 4, 5};
     ASSERT_EQ(result.get(), expected);
 }
 
-// Tasks with dependencies (simulate a simple continuation)
-TEST(ThreadPoolTests, TaskDependencies) {
-    ThreadPool pool(4);
-    std::future<int> initialTask = pool.enqueue([]() { return 42; });
-
-    std::future<std::string> dependentTask = pool.enqueue([&initialTask]() {
-        int result = initialTask.get(); // This will wait for the initial task to complete
-        return std::string("Result is ") + std::to_string(result);
-    });
-
-    ASSERT_EQ(dependentTask.get(), "Result is 42");
-}
-
-TEST(ThreadPoolTests, ThreadCount) {
+TYPED_TEST(ThreadPoolTest, ThreadCount)
+{
     std::atomic<int> counter{0};
-    ThreadPool pool(4);
     for (int i = 0; i < 100; ++i) {
-        pool.enqueue([&counter]() {
+        this->pool.enqueue([&counter]() {
             counter.fetch_add(1, std::memory_order_relaxed);
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         });
     }
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait for some tasks to complete
-    ASSERT_GE(counter.load(), 4); // This tests that at least 4 tasks have been processed concurrently
+    // Wait for some tasks to complete
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // This tests that at least 4 tasks have been processed concurrently
+    ASSERT_GE(counter.load(), 4);
+}
+
+// Tasks with dependencies (simulate a simple continuation, but not use continue_with)
+TYPED_TEST(ThreadPoolTest, TaskDependencies)
+{
+    std::future<int> initial_task = this->pool.enqueue([]() { return 42; });
+
+    std::future<std::string> dependent_task =
+        this->pool.enqueue([&initial_task]() {
+            // This will wait for the initial task to complete
+            int result = initial_task.get();
+            return std::string("Result is ") + std::to_string(result);
+        });
+
+    ASSERT_EQ(dependent_task.get(), "Result is 42");
+}
 }
