41692ca7f44b0b40b5e91399e5f4fac78f1d10c4
Ivan Morozko <ivan.morozko01@gmail.com>
2024-05-15 21:29:13 +0300

    [04.ExamSystem]: Add an ability to use different implementations based on env variable

--- ExamSystem/src/main/java/com/example/AppConfig.java
+++ ExamSystem/src/main/java/com/example/AppConfig.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Value("${SET_IMPL:default}")
+    private String implementation;
+
     @Bean
     public ExamSystem examSystem() {
-        return new OptimisticExamSystem();
+        if ("lazy".equalsIgnoreCase(implementation)) {
+            return new LazyExamSystem();
+        } else {
+            return new OptimisticExamSystem();
+        }
     }
 }
